39337f1b189d069498c1d0b68b3827fa5cb91344
Jacob Barber <barber.jacobervin@gmail.com>
2024-04-20 11:14:56 -0500

    Added additional color overrides

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -5,20 +5,32 @@ export const typography = {
   }
 }
 
-// Define color overrides for the accent colors
+// Define color overrides
 const colors = {
   primary: {
-    main: '#dc8522'
+    main: '#c2720a'
   },
   secondary: {
-    main: '#2279dc'
+    main: '#0a5ac2'
+  },
+  error: {
+    main: '#c2160a'
+  },
+  warning: {
+    main: '#b6c20a'
+  },
+  info: {
+    main: '#0ac272'
+  },
+  success: {
+    main: '#5ac20a'
   }
 };
 
-// Build a new palette object for the theme based on the passed ib mode
+// Build a new palette object for the theme based on the passed in mode
 export const palette = (mode: 'light' | 'dark') => ({
   palette: {
-    ...colors,
-    mode
+    mode,
+    ...colors
   }
 })
