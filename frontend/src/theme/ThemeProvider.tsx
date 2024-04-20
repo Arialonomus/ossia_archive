68f9df56c59f9391adeb2738b3851ef5f8c59b16
Jacob Barber <barber.jacobervin@gmail.com>
2024-04-20 10:11:18 -0500

    Added local storage of dark mode state

--- frontend/src/theme/ThemeProvider.tsx
+++ frontend/src/theme/ThemeProvider.tsx
@@ -1,27 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { palette, typography } from './theme.ts';
 import { createTheme, useMediaQuery } from '@mui/material';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
-import { DarkModeContext } from './DarkModeContext.tsx';
+import { DarkModeContext } from '../contexts/DarkModeContext.tsx';
 
 interface ThemeProviderProps {
   children: React.ReactNode
 }
 
 function ThemeProvider({ children }: ThemeProviderProps) {
+  // Query the browser state to see if dark mode is set at the system level
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [mode, setMode] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
-  const theme = createTheme({
+
+  // Initialization of state with either local storage or system preference
+  const [mode, setMode] = useState<'light' | 'dark'>(
+    localStorage.getItem('themeMode')
+      ? localStorage.getItem('themeMode') as 'light' | 'dark'
+      : prefersDarkMode ? 'dark' : 'light'
+  );
+
+  // Update local storage whenever the mode changes
+  useEffect(() => {
+    localStorage.setItem('themeMode', mode);
+  }, [mode]);
+
+  // Create a theme instance dynamically based on the current mode
+  const theme = useMemo(() => createTheme({
     ...typography,
     ...palette(mode)
-  })
+  }), [mode])
 
+  // Function for toggling dark mode in components
   const toggleDarkMode = () => {
     setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
   }
 
   return (
-    <DarkModeContext.Provider value={{ toggleDarkMode, mode }}>
+    <DarkModeContext.Provider value={{ toggleDarkMode }}>
       <MUIThemeProvider theme={theme}>
         {children}
       </MUIThemeProvider>
