d195923453326da22fb657cf039242d323f01070
Jacob Barber <barber.jacobervin@gmail.com>
2024-04-20 10:10:21 -0500

    Migrated to contexts folder

--- frontend/src/theme/DarkModeContext.tsx
+++
@@ -1,8 +0,0 @@
-import {createContext, useContext} from 'react';
-
-export const DarkModeContext = createContext({
-  toggleDarkMode: () => {},
-  mode: 'light'
-})
-
-export const useDarkModeContext = () => useContext(DarkModeContext)
